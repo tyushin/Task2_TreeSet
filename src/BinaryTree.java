bb2e607f272909fd55246c06f5c2f215def5e24f
tyushin <artart998@gmail.com>
2017-11-12 16:28:11 +0300

    add SubSet, HeadSet, TailSet.

---
+++ src/BinaryTree.java
@@ -0,0 +1,201 @@
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.*;
+
+
+// Attention: comparable supported but comparator is not
+@SuppressWarnings("WeakerAccess")
+public class BinaryTree<T extends Comparable<T>> extends AbstractSet<T> implements SortedSet<T> {
+
+    private static class Node<T> {
+        final T value;
+
+        Node<T> left = null;
+
+        Node<T> right = null;
+
+        Node(T value) {
+            this.value = value;
+        }
+    }
+
+    private Node<T> root = null;
+
+    private int size = 0;
+
+    @Override
+    public boolean add(T t) {
+        Node<T> closest = find(t);
+        int comparison = closest == null ? -1 : t.compareTo(closest.value);
+        if (comparison == 0) {
+            return false;
+        }
+        Node<T> newNode = new Node<>(t);
+        if (closest == null) {
+            root = newNode;
+        }
+        else if (comparison < 0) {
+            assert closest.left == null;
+            closest.left = newNode;
+        }
+        else {
+            assert closest.right == null;
+            closest.right = newNode;
+        }
+        size++;
+        return true;
+    }
+
+    boolean checkInvariant() {
+        return root == null || checkInvariant(root);
+    }
+
+    private boolean checkInvariant(Node<T> node) {
+        Node<T> left = node.left;
+        if (left != null && (left.value.compareTo(node.value) >= 0 || !checkInvariant(left))) return false;
+        Node<T> right = node.right;
+        return right == null || right.value.compareTo(node.value) > 0 && checkInvariant(right);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        @SuppressWarnings("unchecked")
+        T t = (T) o;
+        Node<T> closest = find(t);
+        return closest != null && t.compareTo(closest.value) == 0;
+    }
+
+    private Node<T> find(T value) {
+        if (root == null) return null;
+        return find(root, value);
+    }
+
+    private Node<T> find(Node<T> start, T value) {
+        int comparison = value.compareTo(start.value);
+        if (comparison == 0) {
+            return start;
+        }
+        else if (comparison < 0) {
+            if (start.left == null) return start;
+            return find(start.left, value);
+        }
+        else {
+            if (start.right == null) return start;
+            return find(start.right, value);
+        }
+    }
+
+    public class BinaryTreeIterator implements Iterator<T> {
+
+        private Node<T> current = null;
+
+        private BinaryTreeIterator() {}
+
+        private Node<T> findNext() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return findNext() != null;
+        }
+
+        @Override
+        public T next() {
+            current = findNext();
+            if (current == null) throw new NoSuchElementException();
+            return current.value;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    @NotNull
+    @Override
+    public Iterator<T> iterator() {
+        return new BinaryTreeIterator();
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+
+    @Nullable
+    @Override
+    public Comparator<? super T> comparator() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public SortedSet<T> subSet(T fromElement, T toElement) {
+        if (fromElement == null || toElement == null || fromElement.compareTo(toElement) > 0) {
+            throw new IllegalArgumentException();
+        }
+        SortedSet<T> resultSet = new TreeSet<>();
+        addNodes(root,resultSet,fromElement,toElement);
+        return resultSet;
+    }
+
+    private void addNodes(Node<T> actualNode, SortedSet<T> resultSet, T fromElement, T toElement) {
+        if (actualNode != null) {
+            if (fromElement.compareTo(actualNode.value) <= 0 && toElement.compareTo(actualNode.value) >= 0) {
+                resultSet.add(actualNode.value);
+            }
+            if (fromElement.compareTo(actualNode.value) < 0) {
+                addNodes(actualNode.left, resultSet, fromElement, toElement);
+            }
+            if (toElement.compareTo(actualNode.value) > 0) {
+                addNodes(actualNode.right, resultSet, fromElement, toElement);
+            }
+        }
+        else {
+            return;
+        }
+    }
+
+    @NotNull
+    @Override
+    public SortedSet<T> headSet(T toElement) {
+        SortedSet<T> resultSet;
+        return resultSet = subSet(first(), toElement);
+    }
+
+    @NotNull
+    @Override
+    public SortedSet<T> tailSet(T fromElement) {
+        SortedSet<T> resultSet;
+        return resultSet =subSet(fromElement, last());
+    }
+
+    @Override
+    public T first() {
+        if (root == null) throw new NoSuchElementException();
+        Node<T> current = root;
+        while (current.left != null) {
+            current = current.left;
+        }
+        return current.value;
+    }
+
+    @Override
+    public T last() {
+        if (root == null) throw new NoSuchElementException();
+        Node<T> current = root;
+        while (current.right != null) {
+            current = current.right;
+        }
+        return current.value;
+    }
+}
---
+++ src/Tests.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class Tests {
+
+    @Test
+    public void testSubSet() {
+        BinaryTree tree1 = new BinaryTree();
+        List<Integer> nodes = Arrays.asList(1, 3, 4, 6, 8, 12);
+        tree1.addAll(nodes);
+
+        Set<Integer> result1 = new TreeSet<>(Arrays.asList(3, 4, 6, 8));
+        SortedSet<Integer> example1 = tree1.subSet(2, 9);
+
+        Set<Integer> result2 = new TreeSet<>(Arrays.asList(1, 3, 4));
+        SortedSet<Integer> example2 = tree1.subSet(-2, 4);
+
+        Set<Integer> result3 = new TreeSet<>(Arrays.asList(8, 12));
+        SortedSet<Integer> example3 = tree1.subSet(8, 15);
+
+        assertEquals(example1, result1);
+        assertEquals(example2, result2);
+        assertEquals(example3, result3);
+    }
+
+    @Test
+    public void testHeadSet() {
+        BinaryTree tree2 = new BinaryTree();
+        List<Integer> nodes = Arrays.asList(1, 2, 4, 7, 9);
+        tree2.addAll(nodes);
+
+        Set<Integer> result1 = new TreeSet<>(Arrays.asList(1, 2, 4, 7));
+        SortedSet<Integer> example1 = tree2.headSet(7);
+
+        Set<Integer> result2 = new TreeSet<>(Arrays.asList(1, 2, 4));
+        SortedSet<Integer> example2 = tree2.headSet(6);
+
+        assertEquals(example1, result1);
+        assertEquals(example2, result2);
+    }
+
+    @Test
+    public void testTailSet() {
+        BinaryTree tree3 = new BinaryTree();
+        List<Integer> nodes = Arrays.asList(1, 6, 7, 9, 11);
+        tree3.addAll(nodes);
+
+        Set<Integer> result1 = new TreeSet<>(Arrays.asList(7, 9, 11));
+        SortedSet<Integer> example1 = tree3.tailSet(7);
+
+        Set<Integer> result2 = new TreeSet<>(Arrays.asList(9, 11));
+        SortedSet<Integer> example2 = tree3.tailSet(8);
+
+        assertEquals(example1, result1);
+        assertEquals(example2, result2);
+    }
+
+}
